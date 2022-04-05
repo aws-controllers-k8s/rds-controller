46ca0913c05e438751f74336cbfb9a1b625e7fc4
ci-robot <ci-robot@k8s.io>
2022-04-05 21:59:10 +0000

    Update to ACK runtime `v0.18.3`, code-generator `v0.18.3`

--- pkg/resource/db_cluster_parameter_group/sdk.go
+++ pkg/resource/db_cluster_parameter_group/sdk.go
@@ -17,6 +17,7 @@ package db_cluster_parameter_group
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"strings"
 
@@ -387,8 +388,8 @@ func (rm *resourceManager) updateConditions(
 			syncCondition = condition
 		}
 	}
-
-	if rm.terminalAWSError(err) || err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound {
+	var termError *ackerr.TerminalError
+	if rm.terminalAWSError(err) || err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound || errors.As(err, &termError) {
 		if terminalCondition == nil {
 			terminalCondition = &ackv1alpha1.Condition{
 				Type: ackv1alpha1.ConditionTypeTerminal,
@@ -396,7 +397,7 @@ func (rm *resourceManager) updateConditions(
 			ko.Status.Conditions = append(ko.Status.Conditions, terminalCondition)
 		}
 		var errorMessage = ""
-		if err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound {
+		if err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound || errors.As(err, &termError) {
 			errorMessage = err.Error()
 		} else {
 			awsErr, _ := ackerr.AWSError(err)
