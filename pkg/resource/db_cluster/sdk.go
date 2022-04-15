3ac4229b64a23ed0ffae193794b5303c93c55708
ci-robot <ci-robot@k8s.io>
2022-04-15 17:51:52 +0000

    Update to ACK runtime `v0.18.4`, code-generator `v0.18.4`

--- pkg/resource/db_cluster/sdk.go
+++ pkg/resource/db_cluster/sdk.go
@@ -17,6 +17,7 @@ package db_cluster
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"strings"
 
@@ -1319,8 +1320,8 @@ func (rm *resourceManager) updateConditions(
 			syncCondition = condition
 		}
 	}
-
-	if rm.terminalAWSError(err) || err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound {
+	var termError *ackerr.TerminalError
+	if rm.terminalAWSError(err) || err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound || errors.As(err, &termError) {
 		if terminalCondition == nil {
 			terminalCondition = &ackv1alpha1.Condition{
 				Type: ackv1alpha1.ConditionTypeTerminal,
@@ -1328,7 +1329,7 @@ func (rm *resourceManager) updateConditions(
 			ko.Status.Conditions = append(ko.Status.Conditions, terminalCondition)
 		}
 		var errorMessage = ""
-		if err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound {
+		if err == ackerr.SecretTypeNotSupported || err == ackerr.SecretNotFound || errors.As(err, &termError) {
 			errorMessage = err.Error()
 		} else {
 			awsErr, _ := ackerr.AWSError(err)
