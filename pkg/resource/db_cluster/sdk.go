72a2c3a0700be3b8094a84954e56ce94db5237c6
Jay Pipes <jaypipes@gmail.com>
2022-05-10 10:16:17 -0400

    support tags in DBCluster resources
    Adds support for reading and updating tags for DBCluster resources.
    
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

--- pkg/resource/db_cluster/sdk.go
+++ pkg/resource/db_cluster/sdk.go
@@ -536,6 +536,14 @@ func (rm *resourceManager) sdkFind(
 	}
 
 	rm.setStatusDefaults(ko)
+	if ko.Status.ACKResourceMetadata != nil && ko.Status.ACKResourceMetadata.ARN != nil {
+		resourceARN := (*string)(ko.Status.ACKResourceMetadata.ARN)
+		tags, err := rm.getTags(ctx, *resourceARN)
+		if err != nil {
+			return nil, err
+		}
+		ko.Spec.Tags = tags
+	}
 	if !clusterAvailable(&resource{ko}) {
 		// Setting resource synced condition to false will trigger a requeue of
 		// the resource. No need to return a requeue error here.
