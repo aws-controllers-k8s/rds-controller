b608b63d01757d5d347f8d38b35ab99724ac7410
Tenglong Gu <brucegu@amazon.com>
2022-06-13 22:47:04 +0000

    Ignore several fields for rds modify call when no delta

--- pkg/resource/db_instance/sdk.go
+++ pkg/resource/db_instance/sdk.go
@@ -1692,6 +1692,21 @@ func (rm *resourceManager) sdkUpdate(
 		input.DBSubnetGroupName = nil
 	}
 
+	// RDS will not compare diff value and accept any modify db call
+	// for below values, MonitoringInterval, CACertificateIdentifier
+	// and user master password
+	// hence if there is no delta between desired
+	// and latest, exclude it from ModifyDBInstanceRequest
+	if !delta.DifferentAt("Spec.MonitoringInterval") {
+		input.MonitoringInterval = nil
+	}
+	if !delta.DifferentAt("Spec.CACertificateIdentifier") {
+		input.CACertificateIdentifier = nil
+	}
+	if !delta.DifferentAt("Spec.MasterUserPassword.Name") {
+		input.MasterUserPassword = nil
+	}
+
 	var resp *svcsdk.ModifyDBInstanceOutput
 	_ = resp
 	resp, err = rm.sdkapi.ModifyDBInstanceWithContext(ctx, input)
