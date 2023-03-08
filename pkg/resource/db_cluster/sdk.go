8f1c0abdf1b5a76ef755fb2578b1af696c732350
Arjun Naik <arjunrn@users.noreply.github.com>
2023-03-08 11:53:46 +0100

    Copy the security groups from status into spec
    Because the names of the security group fields returned by the API
    differ the `VPCSecurityGroupsIDs` is not read from the API response into
    the _latest_. Instead the values of the _desired_ is copied. This means
    that the controller determines that there is not need for an update.
    Instead by copying the the VPCSecurityGroups field from the status into
    the spec the controller can correctly determine that there is a
    difference between the latest and desired. This operation is only
    performed when a Security Group has been specified. This affects both
    type __DBCluster__ and __DBInstance__.

--- pkg/resource/db_cluster/sdk.go
+++ pkg/resource/db_cluster/sdk.go
@@ -600,6 +600,18 @@ func (rm *resourceManager) sdkFind(
 	} else {
 		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
 	}
+	if len(r.ko.Spec.VPCSecurityGroupIDs) > 0 {
+		// If the desired resource has security groups specified then update the spec of the latest resource with the
+		// value from the status. This is done so that when a cluster is created without security groups and gets a
+		// default security group attached to it, it is not overwritten with empty security groups from the
+		// desired resource.
+		sgIDs := make([]*string, len(ko.Status.VPCSecurityGroups))
+		for i, sg := range ko.Status.VPCSecurityGroups {
+			id := *sg.VPCSecurityGroupID
+			sgIDs[i] = &id
+		}
+		ko.Spec.VPCSecurityGroupIDs = sgIDs
+	}
 
 	return &resource{ko}, nil
 }
--- templates/hooks/db_instance/sdk_read_many_post_set_output.go.tpl
+++ templates/hooks/db_instance/sdk_read_many_post_set_output.go.tpl
@@ -11,4 +11,15 @@
 		// the resource. No need to return a requeue error here.
 		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
 	}
-
+	if len(r.ko.Spec.VPCSecurityGroupIDs) > 0 {
+		// If the desired resource has security groups specified then update the spec of the latest resource with the
+		// security groups from the status. This is done so that when an instance is created without security groups
+		// and gets a default security group attached to it, it is not overwritten with no security groups from the
+		// desired resource.
+		sgIDs := make([]*string, len(ko.Status.VPCSecurityGroups))
+		for i, sg := range ko.Status.VPCSecurityGroups {
+			id := *sg.VPCSecurityGroupID
+			sgIDs[i] = &id
+		}
+		ko.Spec.VPCSecurityGroupIDs = sgIDs
+	}
