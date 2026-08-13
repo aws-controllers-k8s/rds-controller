12dbf359328737a8e45b4a433fcf44ad85f96b83
Gustavo Diaz <gustidia@amazon.com>
2026-08-13 16:48:46 +0000

    chore: Regenerate with code-generator v0.62.1
    Picks up code-generator v0.62.1, which adds ACKGenerateVersion and
    ACKGenerateBuildDate to pkg/version/version.go and a debug-level build details
    line to cmd/controller/main.go (code-generator #728).
    
    This controller had not been regenerated since that release, so the churn is
    unrelated to any functional change and is separated out to keep the following
    commit reviewable.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -15,8 +15,18 @@
 
 package version
 
+// GitVersion, GitCommit, and BuildDate describe the service controller build
+// and are injected at controller build time via -ldflags.
 var (
 	GitVersion string
 	GitCommit  string
 	BuildDate  string
+)
+
+// ACKGenerateVersion and ACKGenerateBuildDate identify the ack-generate build
+// that generated this controller's code. They are baked in at code-generation
+// time and are immutable for the life of the generated code.
+const (
+	ACKGenerateVersion   = "v0.62.1"
+	ACKGenerateBuildDate = "2026-08-13T16:48:13Z"
 )
